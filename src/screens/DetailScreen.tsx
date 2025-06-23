bff4644977aa7ced55f08fb39a92bfe956e1210f
Spring-Rebirth <cjunwei6249@gmail.com>
2025-06-23 11:02:42 +0800

    practise-2 : 完成查询和预加载

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,9 +1,24 @@
 import { View, Text } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import { useQuery } from '@tanstack/react-query'
+import { postsApi } from '../constants/api'
 
 export default function DetailScreen() {
+  const route = useRoute()
+  const { id } = route.params as { id: string }
+
+  console.log('DetailScreen - id:', id)
+
+  const query = useQuery({
+    queryKey: ['post', id],
+    queryFn: () => postsApi.get(id),
+  })
+
+  console.log('DetailScreen - query.data:', query.data)
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 26, fontWeight: 'bold' }}>DetailScreen</Text>
+      <Text style={{ fontSize: 26, fontWeight: 'bold' }}>{query?.data?.body ?? query?.data?.title}</Text>
     </View>
   )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,20 @@
 import { Text, View, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native'
-import { postsApi, postUrl, PostPayload } from '../constants/api'
+import { postsApi } from '../constants/api'
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, NavigationProp } from '@react-navigation/native'
 
 export default function HomeScreen() {
-    const navigation = useNavigation();
+
+    type RootStackParamList = {
+        Home: undefined;
+        Detail: { id: string };
+    };
+
+    const queryClient = useQueryClient();
+
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
     const query = useQuery({
-        queryKey: ['example'],
+        queryKey: ['posts'],
         queryFn: postsApi.list,
     })
 
@@ -26,19 +34,28 @@ export default function HomeScreen() {
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <ScrollView>
                 {query.data.map((item: any) => (
-                    <Text key={item.id} style={{ fontSize: 16, fontWeight: 'bold' }}>
-                        {item.title}
-                    </Text>
+                    <View key={item.id} style={{ height: 50, width: '100%', justifyContent: 'center', alignItems: 'center', paddingHorizontal: 16, paddingVertical: 16 }}>
+                        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+                            {item.title}
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() => {
+                                queryClient.prefetchQuery({
+                                    queryKey: ['post', item.id],
+                                    queryFn: () => postsApi.get(item.id)
+                                })
+                                navigation.navigate('Detail', { id: item.id })
+                            }}
+                            style={{ width: 160, height: 26, backgroundColor: 'skyblue', justifyContent: 'center', alignItems: 'center' }}
+                        >
+
+                            <Text style={{ color: 'black' }}>Go to Detail</Text>
+                        </TouchableOpacity>
+                    </View>
                 ))}
             </ScrollView>
-            <View style={{ height: 50, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('Detail')}
-                    style={{ width: 160, height: 26, backgroundColor: 'green', justifyContent: 'center', alignItems: 'center' }}
-                >
-                    <Text style={{ color: 'white' }}>Go to Detail</Text>
-                </TouchableOpacity>
-            </View>
+
+
         </View>
     );
 }
